b52ee536209f818110c06c98509692e25d78c9d6
Adrian Madrid <aemadrid@gmail.com>
2015-06-11 14:33:49 -0600

    Added tracking and display of time taken to succeed or fail.

--- lib/tiki/torch/consumers/hooks.rb
+++ lib/tiki/torch/consumers/hooks.rb
@@ -12,18 +12,22 @@ module Tiki
         end
 
         def on_start
+          @start_time = Time.now
           debug "Event ##{short_id} started"
         end
 
         def on_success(result)
-          info "Event ##{short_id} succeeded with #{result.inspect}"
-          @success = result
+          @success_time = Time.now
+          @success      = result
+          info "Event ##{short_id} succeeded with #{result.inspect} in #{success_time_str}"
+
           event.finish
         end
 
         def on_failure(exception)
-          error "Event ##{short_id} failed with #{exception.class.name} : #{exception.message}\n  #{exception.backtrace[0, 5].join("\n  ")}"
-          @failure = exception
+          @failure_time = Time.now
+          @failure      = exception
+          error "Event ##{short_id} failed with #{exception.class.name} : #{exception.message}\n  #{exception.backtrace[0, 5].join("\n  ")}\n in #{failure_time_str}"
 
           back_off_event || dlq_event || finish_event
         end
@@ -42,6 +46,14 @@ module Tiki
         def finish_event
           info "Event ##{short_id} will NOT be requeued ..."
           event.finish
+        end
+
+        def success_time_str
+          Tiki::Torch::Utils.time_taken @start_time, @success_time
+        end
+
+        def failure_time_str
+          Tiki::Torch::Utils.time_taken @start_time, @failure_time
         end
 
       end
